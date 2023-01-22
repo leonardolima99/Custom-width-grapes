863cf2202401f3c3463345d91b005bd1beec7ab2
leonardolima99 <dev.leonardolima@gmail.com>
2023-01-22 10:10:02 -0400

    3 files modified

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log('Hello!');
+const range = document.querySelector('.custom-width-range');
+const number = document.querySelector('.custom-width-number');
+console.log('oooi');
+
+range.addEventListener('input', () => {
+  number.target.value = range.target.value;
+  console.log('hey');
+});
+name = 'custom-width';
